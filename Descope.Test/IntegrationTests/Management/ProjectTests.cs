eecfebbd11ac6b36b991f0b0bf38a32a32ffc433
aviadl <aviad@descope.com>
2024-04-07 15:37:36 +0300

    Split test cases of project api calls

--- Descope.Test/IntegrationTests/Management/ProjectTests.cs
+++ Descope.Test/IntegrationTests/Management/ProjectTests.cs
@@ -13,8 +13,19 @@ namespace Descope.Test.Integration
             await _descopeClient.Management.Project.Import(imported_project);
         }
 
-        [Fact(Skip = "Test fails due to theme import")]
-        public async Task Project_CloneRenameDelete()
+        public async Task Project_Rename()
+        {
+            var name = Guid.NewGuid().ToString().Split("-").First();
+            // Rename
+            var new_name = Guid.NewGuid().ToString().Split("-").First();
+            await _descopeClient.Management.Project.Rename(new_name);
+
+            // Rename again so we will have original name
+            await _descopeClient.Management.Project.Rename("Dotnet SDK Testing");
+        }
+
+        [Fact(Skip = "Skipping due to lack of mgmt key permissions")]
+        public async Task Project_CloneAndDelete()
         {
             // Clone the current project
             var name = Guid.NewGuid().ToString().Split("-").First();
@@ -23,13 +34,6 @@ namespace Descope.Test.Integration
 
             // Delete cloned project
             await _descopeClient.Management.Project.Delete(result.ProjectId);
-
-            // Rename
-            var new_name = Guid.NewGuid().ToString().Split("-").First();
-            await _descopeClient.Management.Project.Rename(new_name);
-
-            // Rename again so we will have original name
-            await _descopeClient.Management.Project.Rename("dotnet");
         }
 
     }
